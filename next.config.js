24e08227a3fa8ac9ed4dd476698629f8d7ecf2d8
mywayapps <mywayapps10@gmail.com>
2025-07-04 08:14:51 +0000

    Added config for resolving file path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-  // config options here
-}
+  // Add any other Next.js config options here
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
